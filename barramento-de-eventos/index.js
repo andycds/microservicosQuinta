42c5c065fecc1f9043bba550605e372b16899f4f
Anderson Sanches <andycds@gmail.com>
2023-04-07 00:45:27 -0300

    Serviço de consultas funcionando

--- barramento-de-eventos/index.js
+++ barramento-de-eventos/index.js
@@ -12,7 +12,9 @@ app.post('/eventos', (req, res) => {
     axios.post('http://localhost:4000/eventos', evento);
     //envia o evento para o microsserviço de observações
     axios.post('http://localhost:5000/eventos', evento);
-    res.send({msg: "ok"});
+    //envia o evento para o microsserviço de consultas
+    axios.post('http://localhost:6000/eventos', evento);
+    res.send({ msg: "ok" });
 });
 
 app.listen(10000, () => {
