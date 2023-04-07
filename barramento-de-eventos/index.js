f91ac1c3150d57f01fb6bd0c71ffd2daa406d1da
Anderson Sanches <andycds@gmail.com>
2023-04-07 00:15:54 -0300

    Barramento de eventos se comunicando com oserviço de lembretes e o de observações

--- barramento-de-eventos/index.js
+++ barramento-de-eventos/index.js
@@ -1,4 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
+
+app.post('/eventos', (req, res) => {
+    const evento = req.body;
+    console.log("Chegou um evento!");
+    console.log(evento);
+    //envia o evento para o microsserviço de lembretes
+    axios.post('http://localhost:4000/eventos', evento);
+    //envia o evento para o microsserviço de observações
+    axios.post('http://localhost:5000/eventos', evento);
+    res.send({msg: "ok"});
+});
+
+app.listen(10000, () => {
+    console.log('Barramento de eventos. Porta 10000.');
+})
--- observacoes/index.js
+++ observacoes/index.js
@@ -1,24 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 const { v4: uuidv4 } = require('uuid');
 
 const observacoesPorLembreteId = {};
-
+ 
 //:id é um placeholder
 //exemplo: /lembretes/123456/observacoes
-app.post('/lembretes/:id/observacoes', (req, res) => {
+app.post('/lembretes/:id/observacoes', async (req, res) => {
     const idObs = uuidv4();
     const { texto } = req.body;
     const observacoesDoLembrete = observacoesPorLembreteId[req.params.id] || [];
     observacoesDoLembrete.push({ id: idObs, texto });
     observacoesPorLembreteId[req.params.id] = observacoesDoLembrete;
+    await axios.post('http://localhost:10000/eventos', {
+        tipo: "ObservacaoCriada",
+        dados: {
+            id: idObs, texto, lembreteId: req.params.id
+        }
+    })
     res.status(201).send(observacoesDoLembrete);
 });
 
 app.get('/lembretes/:id/observacoes', (req, res) => {
     res.send(observacoesPorLembreteId[req.params.id] || []);
+});
+
+app.post("/eventos", (req, res) => {
+    console.log(req.body);
+    res.send({msg: "ok"});
 });
 
 app.listen(5000, (() => {
