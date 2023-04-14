80d6107f467f0b92b4d8bbb3bbcc6df97bcaf832
Anderson Sanches <andycds@gmail.com>
2023-04-14 00:27:10 -0300

    Serviço de Classificação OK

--- observacoes/index.js
+++ observacoes/index.js
@@ -6,19 +6,39 @@ app.use(bodyParser.json());
 const { v4: uuidv4 } = require('uuid');
 
 const observacoesPorLembreteId = {};
- 
+
+const funcoes = {
+    ObservacaoClassificada: (observacao) => {
+        const observacoes = observacoesPorLembreteId[observacao.lembreteId];
+        const obsParaAtualizar = observacoes.find(o => o.id === observacao.id);
+        obsParaAtualizar.status = observacao.status;
+        axios.post("http://localhost:10000/eventos", {
+            tipo: "ObservacaoAtualizada",
+            dados: {
+                id: observacao.id,
+                texto: observacao.texto,
+                lembreteId: observacao.lembreteId,
+                status: observacao.status
+            }
+        });
+    }
+}
+
 //:id é um placeholder
 //exemplo: /lembretes/123456/observacoes
 app.post('/lembretes/:id/observacoes', async (req, res) => {
     const idObs = uuidv4();
     const { texto } = req.body;
     const observacoesDoLembrete = observacoesPorLembreteId[req.params.id] || [];
-    observacoesDoLembrete.push({ id: idObs, texto });
+    observacoesDoLembrete.push({ id: idObs, texto, status: 'aguardando' });
     observacoesPorLembreteId[req.params.id] = observacoesDoLembrete;
     await axios.post('http://localhost:10000/eventos', {
         tipo: "ObservacaoCriada",
         dados: {
-            id: idObs, texto, lembreteId: req.params.id
+            id: idObs, 
+            texto, 
+            lembreteId: req.params.id, 
+            status: 'aguardando'
         }
     })
     res.status(201).send(observacoesDoLembrete);
@@ -29,7 +49,9 @@ app.get('/lembretes/:id/observacoes', (req, res) => {
 });
 
 app.post("/eventos", (req, res) => {
-    console.log(req.body);
+    try {
+        funcoes[req.body.tipo](req.body.dados);
+    } catch (err) {}
     res.send({msg: "ok"});
 });
 
