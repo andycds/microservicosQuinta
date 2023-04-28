62fb8ba80636484322f627a038303b23efe2adb4
Anderson Sanches <andycds@gmail.com>
2023-04-28 00:06:27 -0300

    Microsserviço de consultas se recuperando após ficar fora do ar

--- barramento-de-eventos/index.js
+++ barramento-de-eventos/index.js
@@ -4,21 +4,32 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/eventos', (req, res) => {
+const eventos = [];
+
+app.post('/eventos', async (req, res) => {
     const evento = req.body;
     console.log("Chegou um evento!");
     console.log(evento);
-    //envia o evento para o microsserviço de lembretes
-    axios.post('http://localhost:4000/eventos', evento);
-    //envia o evento para o microsserviço de observações
-    axios.post('http://localhost:5000/eventos', evento);
-    //envia o evento para o microsserviço de consultas
-    axios.post('http://localhost:6000/eventos', evento);
-    //envia o evento para o microsserviço de classificação
-    axios.post('http://localhost:7000/eventos', evento);
+    eventos.push(evento);
+    try {
+        //envia o evento para o microsserviço de lembretes
+        await axios.post('http://localhost:4000/eventos', evento);
+        //envia o evento para o microsserviço de observações
+        await axios.post('http://localhost:5000/eventos', evento);
+        //envia o evento para o microsserviço de consultas
+        await axios.post('http://localhost:6000/eventos', evento);
+        //envia o evento para o microsserviço de classificação
+        await axios.post('http://localhost:7000/eventos', evento);
+    } catch (error) {
+        console.log("Algum serviço está morto.");
+    }
     res.send({ msg: "ok" });
+});
+
+app.get('/eventos', (req, res) => {
+    res.send(eventos);
 });
 
 app.listen(10000, () => {
     console.log('Barramento de eventos. Porta 10000.');
-})
+});
